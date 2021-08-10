237c31be833a7069811e0657c7e3f5c0b0a5ceb3
Elsie Otube <elsie.otube@strathmore.edu>
2021-08-10 20:18:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/implicitintents/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.implicitintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mWebsiteEditText;
+    private EditText mLocationEditText;
+    private EditText mShareTextEditText;
+    private EditText mCallNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebsiteEditText = findViewById(R.id.website_edittext);
+        mLocationEditText = findViewById(R.id.location_edittext);
+        mShareTextEditText = findViewById(R.id.share_edittext);
+        mCallNumber = findViewById(R.id.number_edittext);
+    }
+
+    public void openWebsite(View view) {
+        String url = mWebsiteEditText.getText().toString();
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+
+    }
+
+    public void openLocation(View view) {
+        String loc = mLocationEditText.getText().toString();
+
+        Uri addressUri = Uri.parse(getString(R.string.my_location) + loc);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+
+    public void shareText(View view) {
+        String txt = mShareTextEditText.getText().toString();
+        String mimeType = "text/plain";
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle(R.string.share_text)
+                .setText(txt)
+                .startChooser();
+
+    }
+
+    public void callNumber(View view) {
+        String number = mCallNumber.getText().toString();
+        Uri callNo = Uri.parse("tel:"+number);
+        Intent intent = new Intent(Intent.ACTION_DIAL, callNo);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+
+
+    }
+}
